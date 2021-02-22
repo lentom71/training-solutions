8421e8bbf67f27e579087903b4f598d7b7c7bc91
Lengyel Tamás <lentom71@gmail.com>
2021-02-22 19:01:48 +0100

    Exam03_Cdv

--- src/main/java/exam03_potvizsga/CdvCheck.java
+++ src/main/java/exam03_potvizsga/CdvCheck.java
@@ -22,3 +22,12 @@ public class CdvCheck
         }
     }
 }
+
+ /*   Írj egy CdvCheck.check() metódust (paraméterezése, visszatérési értéke kiderül a tesztesetből), mely kap egy adószámot, és eldönti róla, hogy helyes-e! A következőképp kell eldönteni. Fogni kell az első kilenc számjegyet, és megszorozni rendre 1, 2, ..., 9 számmal. Az eredményt kell összegezni, majd maradékos osztani 11-el. A 10. számjegynek meg kell egyeznie ezzel a számmal (maradékkal).
+
+        Pl.:
+
+        8365670003
+        1 * 8 + 2 * 3 + 3 * 6 + ... + 9 * 0 = 124
+        124 maradékos osztás 11-el = 3
+        3 (maradék) == 3 (10. számhegy) - az adószám megfelelő   */
